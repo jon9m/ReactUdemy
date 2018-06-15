c1286e4af788026d3dc06419acd1f59921e07916
jon <manojam79@gmail.com>
2018-06-15 12:36:27 +1000

    Pass methods as props

--- src/App.js
+++ src/App.js
@@ -2,8 +2,37 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
+//Person must start with a capital letter
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'manoj', age: 38 },
+      { name: 'sam', age: 37 },
+      { name: 'nilupa', age: 33 }
+    ]
+  }
+
+  changeNamehandler = () => {
+    //this.state.persons[0].name = "Malaka"; //Don't do this
+    this.setState({
+      persons: [
+      { name: 'malaka', age: 36 },
+      { name: 'samara', age: 37 },
+      { name: 'nilupa thushari', age: 30 }
+    ]
+    })
+  }
+  resetNamehandler = () => {
+    this.setState({
+      persons: [
+      { name: 'manoj', age: 38 },
+      { name: 'sam', age: 37 },
+      { name: 'nilupa', age: 33 }
+    ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,12 +43,15 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button className="btn btn-info" onClick={this.changeNamehandler}>Switch Name</button>
 
+        <Person name="sam" age="38"></Person>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My name is jon!!!</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
+        <p>Invoke component click from outside</p>
+        <Person clickme={this.resetNamehandler} name={this.state.persons[2].name} age={this.state.persons[2].age}><button className="btn btn-warning">Click Me</button></Person>
 
-        <Person></Person>
       </div>
-
-      
     );
   }
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,17 @@
 import React from 'react';
-const person = () =>{
-    return <p>Hi from a function</p>
+const person = (props) => {
+    let style = {
+        width: '30rem',
+        margin: '5px auto',
+    }
+
+    return (
+        <div onClick={props.clickme} className="card my7" style={style}>
+            <p className="card-title">Hi from a function to {props.name}</p>
+            <span className="card-text">my age is {props.age}</span>
+            <p className="card-text">{props.children}</p>
+        </div>
+    );
 }
 
 export default person;
